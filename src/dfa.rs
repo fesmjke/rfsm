cb064fdea8962914019264963bea562bf5be8261
fesmjke <fatalerror.sm@gmail.com>
2023-04-29 21:17:37 +0300

    fmt: formated with rust fmt, removed table field and method

--- src/dfa.rs
+++ src/dfa.rs
@@ -1,35 +1,25 @@
-use std::vec;
-
-#[derive(Debug,PartialEq,Clone, Copy)]
-enum Action {
-    Coin,
-    Push
-}
-
-#[derive(Debug,Clone,PartialEq)]
-enum Trunstile {
-    Locked,
-    Unlocked
-}
 #[derive(Debug)]
 enum TransitionError {
     MissingCondition,
-    Unknown
+    Unknown,
 }
 
 #[derive(Debug)]
-struct Transition<T, A>{
+struct Transition<T, A> {
     from: T,
     to: T,
-    condition: A // todo
+    condition: A,
 }
 
-impl<T, A> Transition<T, A>  where T: Clone {
-    fn from(fst : T, scd: T, condition: A) -> Transition<T, A>  {        
+impl<T, A> Transition<T, A>
+where
+    T: Clone,
+{
+    fn from(fst: T, scd: T, condition: A) -> Transition<T, A> {
         Self {
-            from : fst,
-            to : scd,
-            condition, // todo
+            from: fst,
+            to: scd,
+            condition,
         }
     }
 }
@@ -38,16 +28,18 @@ struct State<T, A> {
     state: T,
     action: A,
     transitions: Vec<Transition<T, A>>,
-    table: Vec<Vec<T>> // todo
 }
 
-impl<T: Clone + std::cmp::PartialEq + std::fmt::Debug, A: std::cmp::PartialEq + Copy + std::fmt::Debug> State<T, A> {
-    fn new(init_state : T, init_action: A) -> State<T, A> {
+impl<T, A> State<T, A>
+where
+    T: std::cmp::PartialEq + Clone + std::fmt::Debug,
+    A: std::cmp::PartialEq + Copy + std::fmt::Debug,
+{
+    fn new(init_state: T, init_action: A) -> State<T, A> {
         Self {
-            state : init_state,
-            action : init_action,
-            transitions : vec![],
-            table: vec![]
+            state: init_state,
+            action: init_action,
+            transitions: vec![]
         }
     }
 
@@ -55,21 +47,17 @@ impl<T: Clone + std::cmp::PartialEq + std::fmt::Debug, A: std::cmp::PartialEq + 
         self.transitions.push(transition);
     }
 
-    fn build_table(&self) {  
-        todo!();
-    }
-
-    fn next(&mut self, input : A) -> Result<String, TransitionError> {
-        let conditions : Vec<_> = self.transitions.iter().map(|tr| tr.condition).collect();
+    fn next(&mut self, input: A) -> Result<T, TransitionError> {
+        let conditions: Vec<_> = self.transitions.iter().map(|tr| tr.condition).collect();
 
         if !conditions.contains(&input) {
             return Err(TransitionError::MissingCondition);
         }
-
+        
         for transitions in self.transitions.iter() {
             if transitions.from == self.state && transitions.condition == input {
                 self.state = transitions.to.clone();
-                return Ok(format!("state changed to {:?}", self.state));
+                return Ok(self.state.clone());
             }
         }
 
@@ -80,29 +68,6 @@ impl<T: Clone + std::cmp::PartialEq + std::fmt::Debug, A: std::cmp::PartialEq + 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn trunstile_generic() {
-        //    C - coin
-        // L --> U
-        //    P - push
-        // L --> L 
-        //    P - push
-        // U --> L
-        //    C - coin
-        // U --> U
-
-        let f_t = Transition::from(Trunstile::Locked, Trunstile::Unlocked, Action::Coin);
-        let s_t = Transition::from(Trunstile::Unlocked, Trunstile::Locked, Action::Push);
-
-        let mut state = State::new(Trunstile::Locked,Action::Push);
-
-        state.add_transition(f_t);
-        state.add_transition(s_t);
-
-        assert_eq!(state.state, Trunstile::Locked);
-    }
-
     #[test]
     fn generic() {
         //        b
@@ -162,7 +127,7 @@ mod tests {
 
         match state.next('a') {
             Ok(answer) => println!("{:?}", answer),
-            Err(why) => println!("{:?}",why),
+            Err(why) => println!("{:?}", why),
         }
     }
 }
